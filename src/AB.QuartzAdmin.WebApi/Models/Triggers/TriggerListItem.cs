5d65f1e7298f495842dd3f2a34ae8f4fecb46fd7
gwinnem <gwinnem@gmail.com>
2019-09-16 10:21:43 +0200

    xml comments

--- src/AB.QuartzAdmin.WebApi/Models/Triggers/TriggerListItem.cs
+++ src/AB.QuartzAdmin.WebApi/Models/Triggers/TriggerListItem.cs
@@ -1,19 +1,51 @@
-﻿namespace AB.QuartzAdmin.WebApi.Models.Triggers
+﻿using Quartz;
+
+namespace AB.QuartzAdmin.WebApi.Models.Triggers
 {
     /// <summary>
     /// Data model for a trigger list item.
     /// </summary>
     public sealed class TriggerListItem
     {
+        /// <summary>
+        /// The Name of the <see cref="ITrigger"/>
+        /// </summary>
         public string TriggerName { get; set; }
+        /// <summary>
+        /// The Trigger Group where the <see cref="ITrigger"/> is configured with.
+        /// </summary>
         public string TriggerGroup { get; set; }
+        /// <summary>
+        /// True if trigger is paused.
+        /// </summary>
         public bool IsPaused { get; set; }
+        /// <summary>
+        /// The <see cref="TriggerType"/> type of trigger.
+        /// </summary>
         public TriggerType Type { get; set; }
+        /// <summary>
+        /// Short description of the <see cref="ITrigger"/>
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// The time when the trigger fired first time.
+        /// </summary>
         public string StartTime { get; set; }
+        /// <summary>
+        /// The time when a trigger will stop triggering,
+        /// </summary>
         public string EndTime { get; set; }
+        /// <summary>
+        /// The last time the trigger was fired.
+        /// </summary>
         public string LastFireTime { get; set; }
+        /// <summary>
+        /// The next time the trigger fires.
+        /// </summary>
         public string NextFireTime { get; set; }
+        /// <summary>
+        /// Human readable description of the <see cref="ITrigger"/> scheduling settings.
+        /// </summary>
         public string ScheduleDescription { get; set; }
     }
 }
